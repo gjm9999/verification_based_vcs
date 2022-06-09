5c392df134c4c9e547b791432e30e00b77d0ec84
gjm9999 <gaojiaming1@outlook.com>
2022-06-09 11:39:44 -0400

    update

--- vcs_demo/cfg/verdiLog/verdi.cmd
+++
@@ -1,8 +0,0 @@
-debImport "-f" "tb.f"
-verdiDockWidgetDisplay -dock widgetDock_WelcomePage
-verdiDockWidgetHide -dock widgetDock_WelcomePage
-srcHBSelect "testbench" -win $_nTrace1
-srcSetScope -win $_nTrace1 "testbench" -delim "."
-srcHBSelect "testbench.bus" -win $_nTrace1
-srcSetScope -win $_nTrace1 "testbench.bus" -delim "."
-debExit
